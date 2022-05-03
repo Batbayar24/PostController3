2e41a461fca9ba45d1d2813d986184b517415eb4
Batbayar24 <lbayaraa24@gmail.com>
2022-05-04 02:48:45 +0800

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $allPost = Post::all();
+        return response()->json($allPost);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->p_image = $request->p_image;
+        $post->user_id = $request->user_id;
+        $post->save();
+        return response()->json([
+            'message'=>'post amjilttai orloo',
+            'status'=>'200'
+
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return response()->json($post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        // $post->title = $request->title;
+        // $post->description = $request->description;
+        // $post->p_image = $request->p_image;
+        // $post->user_id = $request->user_id;
+        $post->update($request->all());
+        return response()->json([
+            'message'=>'post amjilttai shinechlegdlee',
+            'status'=>'200'
+
+        ]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json([
+            'message'=>'Post amjilttai ustgagdlaa',
+            'status'=>'200'
+        ]);
+    }
+}
